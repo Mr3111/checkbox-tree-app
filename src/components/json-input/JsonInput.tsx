ceb838487b9f40f5aeb221748272ddc1d1010a93
Rishab Mishra <rishab.mishra@bfhl.in>
2022-08-22 18:46:34 +0530

    added zod validation and relevant error message

--- src/components/json-input/JsonInput.tsx
+++ src/components/json-input/JsonInput.tsx
@@ -1,24 +1,43 @@
-import { Button, Input, Space } from 'antd';
+import { Button, Input, Space, Typography } from 'antd';
 import React, { useState } from 'react';
+import { z } from 'zod';
 const { TextArea } = Input;
+const { Text } = Typography;
+
+const INode = z.array(
+    z.object({
+        name: z.string(),
+        parentId: z.string().nullable(),
+    })
+);
 
 type JsonInputProps = {
     handleSubmit: (value: string) => void;
 };
 
+type IError = {
+    status: 'error' | 'warning' | '';
+    message?: string;
+};
+
 const JsonInput = ({ handleSubmit }: JsonInputProps) => {
-    const [error, setError] = useState<'error' | ''>('');
+    const [error, setError] = useState<IError>();
     const [value, setValue] = useState<string>('');
     function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
         const value = e.target.value;
         setValue(value);
         try {
-            JSON.parse(value);
-            // setValue(JSON.stringify(JSON.parse(value), null, 4));
-            setError('');
-        } catch (e) {
-            // setValue(value);
-            setError('error');
+            const data = JSON.parse(value);
+            INode.parse(data);
+            setError({ status: '' });
+        } catch (e: any) {
+            setError({
+                message:
+                    e.name === 'SyntaxError'
+                        ? e.message
+                        : 'This JSON cannot be converted to tree',
+                status: e.name === 'SyntaxError' ? 'error' : 'warning',
+            });
             return false;
         }
     }
@@ -26,19 +45,26 @@ const JsonInput = ({ handleSubmit }: JsonInputProps) => {
     function handleBeautify() {
         setValue(JSON.stringify(JSON.parse(value), null, 4));
     }
+    // @ts-ignore
     return (
         <Space direction="vertical">
             <TextArea
                 value={value}
-                status={error}
+                status={error?.status}
                 onChange={handleChange}
                 placeholder="Enter/ Paste JSON data here..."
                 autoSize
             />
+            {error?.status && (
+                <Text type={error.status === 'error' ? 'danger' : error.status}>
+                    {error.message}
+                </Text>
+            )}
+
             <Space direction="horizontal">
                 <Button
                     type="ghost"
-                    disabled={!!error}
+                    disabled={error?.status === 'error'}
                     onClick={handleBeautify}
                 >
                     Beautify 🪄
